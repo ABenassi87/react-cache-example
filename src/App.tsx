46bf22e6946e3fc7d22e04fe9d1038c21268bf87
Alfredo Benassi <alfrebenassi@gmail.com>
2020-07-19 09:22:17 -0300

    Update Notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './assets/main.css';
 import Dashboard from './components/Dashboard/Dashboard';
-import * as serviceWorker from './serviceWorker';
-import { withSnackbar } from 'notistack';
 
-const App: React.FunctionComponent = (props: any) => {
-  const { enqueueSnackbar } = props;
-  const [waitingWorker, setWaitingWorker] = useState<any>({});
-  const [newVersionAvailable, setNewVersionAvailable] = useState<boolean>(false);
-
-  useEffect(() => {
-    serviceWorker.register({ onUpdate: onServiceWorkerUpdate });
-    if (newVersionAvailable)
-      //show snackbar with refresh button
-      enqueueSnackbar('A new version was released', {
-        persist: true,
-        variant: 'success',
-        action: refreshAction(),
-      });
-  }, []);
-
-  const onServiceWorkerUpdate = (registration: ServiceWorkerRegistration) => {
-    setWaitingWorker(registration && registration.waiting);
-    setNewVersionAvailable(true);
-  };
-
-  const updateServiceWorker = () => {
-    waitingWorker && waitingWorker.postMessage({ type: 'SKIP_WAITING' });
-    setNewVersionAvailable(false);
-    window.location.reload();
-  };
-
-  const refreshAction = () => {
-    //render the snackbar button
-    return (
-      <React.Fragment>
-        <button className='snackbar-button' onClick={updateServiceWorker}>
-          {'refresh'}
-        </button>
-      </React.Fragment>
-    );
-  };
-
+const App: React.FunctionComponent = () => {
   return (
     <div className='App'>
       <Dashboard />
@@ -49,4 +10,4 @@ const App: React.FunctionComponent = (props: any) => {
   );
 };
 
-export default withSnackbar(App);
+export default App;
---
+++ src/components/UpdateNotification/UpdateNotification.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import * as serviceWorker from '../../serviceWorker';
+import { Button, Snackbar } from '@material-ui/core';
+
+const UpdateNotification: React.FunctionComponent = () => {
+  const [showReload, setShowReload] = useState(false);
+  const [waitingWorker, setWaitingWorker] = useState<ServiceWorker | null>(null);
+  const onSWUpdate = (registration: ServiceWorkerRegistration) => {
+    setShowReload(true);
+    setWaitingWorker(registration.waiting);
+  };
+
+  useEffect(() => {
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.register({ onUpdate: onSWUpdate });
+  }, []);
+
+  const reloadPage = () => {
+    waitingWorker?.postMessage({ type: 'SKIP_WAITING' });
+    setShowReload(false);
+    window.location.reload();
+  };
+
+  return (
+    <Snackbar
+      open={showReload}
+      message='A new version is available!'
+      onClick={reloadPage}
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      action={
+        <Button color='inherit' size='small' onClick={reloadPage}>
+          Reload
+        </Button>
+      }
+    />
+  );
+};
+
+export default UpdateNotification;
