b6fd6b884380b3f633da54638dae929871cc34d0
Alfredo Benassi <alfrebenassi@gmail.com>
2020-07-18 21:21:35 -0300

    Apply Update notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './assets/main.css';
 import Dashboard from './components/Dashboard/Dashboard';
+import * as serviceWorker from './serviceWorker';
+import { withSnackbar } from 'notistack';
 
-function App() {
+const App: React.FunctionComponent = (props: any) => {
+  const { enqueueSnackbar } = props;
+  const [waitingWorker, setWaitingWorker] = useState<any>({});
+  const [newVersionAvailable, setNewVersionAvailable] = useState<boolean>(false);
+
+  useEffect(() => {
+    serviceWorker.register({ onUpdate: onServiceWorkerUpdate });
+    if (newVersionAvailable)
+      //show snackbar with refresh button
+      enqueueSnackbar('A new version was released', {
+        persist: true,
+        variant: 'success',
+        action: refreshAction(),
+      });
+  }, []);
+
+  const onServiceWorkerUpdate = (registration: ServiceWorkerRegistration) => {
+    setWaitingWorker(registration && registration.waiting);
+    setNewVersionAvailable(true);
+  };
+
+  const updateServiceWorker = () => {
+    waitingWorker && waitingWorker.postMessage({ type: 'SKIP_WAITING' });
+    setNewVersionAvailable(false);
+    window.location.reload();
+  };
+
+  const refreshAction = () => {
+    //render the snackbar button
+    return (
+      <React.Fragment>
+        <button className='snackbar-button' onClick={updateServiceWorker}>
+          {'refresh'}
+        </button>
+      </React.Fragment>
+    );
+  };
+
   return (
     <div className='App'>
       <Dashboard />
     </div>
   );
-}
+};
 
-export default App;
+export default withSnackbar(App);
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {SnackbarProvider} from "notistack";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider maxSnack={3}>
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+//serviceWorker.register();
